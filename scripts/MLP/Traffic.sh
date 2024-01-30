c6e840a27e48bd19b56c1f10f553c92724bb3c86
qiuyueli <146685418+qiuyueli123@users.noreply.github.com>
2024-01-30 20:35:46 +0800

    Add files via upload

---
+++ scripts/MLP/Traffic.sh
@@ -0,0 +1,552 @@
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 0.0  \
+  --convd_coef 0.0 \
+  --kernel_size 1
+
+
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 0.0  \
+  --convd_coef 0.0 \
+  --kernel_size 3
+
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 0.0  \
+  --convd_coef 0.0 \
+  --kernel_size 1
+
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 0.0  \
+  --convd_coef 0.0 \
+  --kernel_size 3
+  
+
+
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 1.0  \
+  --convd_coef 0.0 \
+  --kernel_size 1
+
+  
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 1.0  \
+  --convd_coef 0.0 \
+  --kernel_size 3
+
+
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 0.0  \
+  --convd_coef 1.0 \
+  --kernel_size 1
+
+
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 0.0  \
+  --convd_coef 1.0 \
+  --kernel_size 3
+
+
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 1.0  \
+  --convd_coef 0.0 \
+  --kernel_size 1
+
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 1.0  \
+  --convd_coef 0.0 \
+  --kernel_size 3
+  
+
+  
+  
+  
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 0.0  \
+  --convd_coef 1.0 \
+  --kernel_size 1
+
+
+  
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 0.0  \
+  --convd_coef 1.0 \
+  --kernel_size 3
+  
+
+  
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 1.0  \
+  --convd_coef 1.0 \
+  --kernel_size 1
+
+  
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 0.0 \
+  --revin_coef 1.0  \
+  --convd_coef 1.0 \
+  --kernel_size 3
+  
+
+   
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 1.0  \
+  --convd_coef 1.0 \
+  --kernel_size 1
+
+
+  
+    python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_720 \
+  --model SCSformer \
+  --data custom \
+  --trans_data True \
+  --date_index 'HourOfDay' 'DayOfWeek' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --d_model 512 \
+  --d_ff 512 \
+  --des 'Exp' \
+  --learning_rate 0.001 \
+  --itr 1 \
+  --train_epochs 16 \
+  --patience 3 \
+  --lradj type6 \
+  --orgin_coef 1.0 \
+  --revin_coef 1.0  \
+  --convd_coef 1.0 \
+  --kernel_size 3
+  
